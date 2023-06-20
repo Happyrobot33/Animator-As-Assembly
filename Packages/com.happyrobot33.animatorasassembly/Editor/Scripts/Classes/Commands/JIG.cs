502752e13f5c35eb6fd0a340e985b6c95c9cf7b9
Happyrobot33 <matthewherber@sbcglobal.net>
2023-06-20 10:35:51 -0400

    functional JIG

--- Packages/com.happyrobot33.animatorasassembly/Editor/Scripts/Classes/Commands/JIG.cs
+++ Packages/com.happyrobot33.animatorasassembly/Editor/Scripts/Classes/Commands/JIG.cs
@@ -10,17 +10,35 @@ namespace AnimatorAsAssembly.Commands
         public Register A;
         public Register B;
         Register Compare;
-        public AacFlState DestState;
+        public string LBLname;
+        AacFlState JumpAway;
+        SUB sub;
 
         /// <summary> Jumps to a LBL if A >= B </summary>
         /// <param name="A"> </param>
         /// <param name="B"> </param>
         /// <param name="LBL"> The LBL to jump to </param>
         /// <param name="Layer"> The FX controller that this command is linked to </param>
-        public JIG(Register A, Register B, AacFlLayer Layer)
+        public JIG(Register A, Register B, string lblname, AacFlLayer Layer)
+        {
+            init(A, B, lblname, Layer);
+        }
+
+        /// <summary> Jumps to a LBL if A >= B </summary>
+        /// <param name="args"> The arguments for the command </param>
+        /// <param name="Layer"> The FX controller that this command is linked to </param>
+        public JIG(string[] args, AacFlLayer Layer)
+        {
+            //split the args into the register and the value
+            init(new Register(args[0], Layer), new Register(args[1], Layer), args[2], Layer);
+        }
+
+        /// <summary> Initialize the variables. This is seperate so multiple constructors can use the same init functionality </summary>
+        void init(Register A, Register B, string lblname, AacFlLayer Layer)
         {
             this.A = A;
             this.B = B;
+            this.LBLname = lblname;
             this.Compare = new Register("INTERNAL/JIG/Compare", Layer);
             this.Layer = Layer;
             states = STATES();
@@ -31,25 +49,44 @@ namespace AnimatorAsAssembly.Commands
             Profiler.BeginSample("JIG");
             AacFlState entry = Layer.NewState("JIG");
             AacFlState exit = Layer.NewState("JIG_EXIT");
-            DestState = Layer.NewState("JIG_DEST");
+            JumpAway = Layer.NewState("JIG_JUMPAWAY");
 
             //if A >= B, jump to LBL
             //do this by checking if A - B is negative
             //if it is, then A < B
             //if it isn't, then A >= B
-            SUB sub = new SUB(A, B, Compare, Layer);
+            sub = new SUB(A, B, Compare, Layer);
             entry.AutomaticallyMovesTo(sub.entry);
 
             //if the highest bit is 1, then A < B
-            sub.exit.TransitionsTo(DestState).When(Compare[Register.bits - 1].IsFalse());
+            sub.exit.TransitionsTo(JumpAway).When(Compare[Register.bits - 1].IsFalse());
             sub.exit.AutomaticallyMovesTo(exit);
 
             Profiler.EndSample();
-            return Util.ConcatArrays(
-                new AacFlState[] { entry },
-                sub.states,
-                new AacFlState[] { DestState, exit }
-            );
+            return Util.ConcatArrays(entry, sub.states, JumpAway, exit);
+        }
+
+        public override void Link(List<OPCODE> opcodes)
+        {
+            //find the LBL
+            foreach (OPCODE opcode in opcodes)
+            {
+                if (opcode.GetType() == typeof(LBL))
+                {
+                    LBL lbl = (LBL)opcode;
+                    if (lbl.name == LBLname)
+                    {
+                        //transition to the LBL
+                        JumpAway.AutomaticallyMovesTo(lbl.entry);
+
+                        //set the program counter to the index of the LBL
+                        JumpAway.Drives(Globals.PROGRAMCOUNTER, opcodes.IndexOf(lbl));
+                        break;
+                    }
+                }
+            }
+
+            base.Link(opcodes);
         }
     }
 }
