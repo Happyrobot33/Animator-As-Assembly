f4d41505eabeffbd3b001b7fc1960345ab0fd4bb
Happyrobot33 <matthewherber@sbcglobal.net>
2023-06-21 15:13:58 -0400

    start work on nested progress bars UI

---
+++ Packages/com.happyrobot33.animatorasassembly/Editor/Scripts/NestedProgressBar.cs
@@ -0,0 +1,79 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnimatorAsAssembly
+{
+    public class NestedProgressBar : EditorWindow
+    {
+        [MenuItem("Debug/Show Progress Bar")]
+        public static void ShowProgressBar()
+        {
+            NestedProgressBar NPB = new NestedProgressBar("Test Window");
+            ProgressBar PB1 = NPB.registerNewProgressBar(
+                "Test Bar Title 1",
+                "Test Bar Description 1"
+            );
+            ProgressBar PB2 = NPB.registerNewProgressBar(
+                "Test Bar Title 2",
+                "Test Bar Description 2"
+            );
+            PB1.progress = 0.5f;
+            NPB.Show();
+
+            PB1.close();
+        }
+
+        private List<ProgressBar> progressBars = new List<ProgressBar>();
+        public string windowTitle;
+
+        public NestedProgressBar(string windowTitle)
+        {
+            titleContent = new GUIContent(windowTitle);
+        }
+
+        void OnGUI()
+        {
+            Repaint();
+            foreach (ProgressBar progressBar in progressBars)
+            {
+                EditorGUILayout.LabelField(progressBar.title);
+                EditorGUILayout.LabelField(progressBar.description);
+                EditorGUILayout.LabelField(progressBar.progress.ToString());
+            }
+        }
+
+        public struct ProgressBar
+        {
+            public string title;
+            public string description;
+            public float progress;
+            internal NestedProgressBar parent;
+
+            public void close()
+            {
+                parent.removeProgressBar(this);
+            }
+        }
+
+        public ProgressBar registerNewProgressBar(string title, string description)
+        {
+            ProgressBar progressBar = new ProgressBar();
+            progressBar.title = title;
+            progressBar.description = description;
+            progressBar.progress = 0;
+            progressBar.parent = this;
+            this.progressBars.Add(progressBar);
+            return progressBar;
+        }
+
+        public void removeProgressBar(ProgressBar progressBar)
+        {
+            progressBars.Remove(progressBar);
+        }
+    }
+}
+
+#endif
